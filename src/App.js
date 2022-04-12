bb23b6810ec81a10db231f82cc45de678304ee77
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-04-12 16:55:00 +0200

    Add readme

--- src/App.js
+++ src/App.js
@@ -19,15 +19,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>The current time is {currentTime}.</p>
+        
       </header>
     </div>
   );
